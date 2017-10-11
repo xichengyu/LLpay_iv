1f71e02c277ba47e80bffb9a1719e83a8e71cb02
xicy <xicy@yintong.com.cn>
2017-10-11 19:37:51 +0800

    modify readdata.py

--- readdata.py
+++ readdata.py
@@ -1,4 +1,13 @@
 # coding=utf-8
 
+import os
+
+
 def read_local_data(localpath):
-	
+	print (os.listdir(localpath))
+	pass
+
+
+if __name__ == "__main__":
+	localpath = "../data_lianlian"
+	read_local_data(localpath)
