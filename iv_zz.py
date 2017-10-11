d0ceba5742dfdeb7e47480c18a150ba537d9b50e
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-11 14:36:16 +0800

    modify iv_zz.py

--- iv_zz.py
+++ iv_zz.py
@@ -11,8 +11,11 @@ from inputs import fetch_from_hive
 import information_value as cal_iv
 
 
-test_sql = 'select count(*) from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
-           'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成")'
+# test_sql = 'select count(*) from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
+#            'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成")'
+
+test_sql = 'select * from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
+           'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成") limit 10'
 
 
 X = np.array(fetch_from_hive(test_sql))
