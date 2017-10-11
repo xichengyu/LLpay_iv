f6bd0358d15a95267e510fcf11036891c687137c
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-11 13:45:51 +0800

    modify iv_zz.py

--- iv_zz.py
+++ iv_zz.py
@@ -3,6 +3,20 @@
 """python 2.7"""
 
 import sys
+import numpy as np
 reload(sys)
 sys.setdefaultencoding('utf-8')
+sys.path.append("../information_value/src")
 
+from inputs import fetch_from_hive
+import information_value
+
+
+test_sql = "select * from dbmodel.data_lianlian_idno_pro limit 10"
+
+
+X = np.array(fetch_from_hive(test_sql))
+
+woe = information_value.WOE()
+
+print (X)
