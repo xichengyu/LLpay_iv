264ee63708d23813cd15ccdb55808fff8257238a
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-11 15:59:25 +0800

    git add information_value.py

--- iv_zz.py
+++ iv_zz.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-"""python 2.7"""
+"""python 3.6"""
 
 import sys
 import numpy as np
@@ -14,12 +14,15 @@ import information_value as cal_iv
 # test_sql = 'select count(*) from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
 #            'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成")'
 
-test_sql = 'select * from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
-           'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成") limit 10'
+# test_sql = 'select * from dbmodel.data_lianlian_idno_pro A inner join dbmodel.loan_data B ' \
+#            'on A.id_no_hash = B.id_no_hash and A.dt_apply = B.dt_apply where B.status in ("已逾期", "还款完成") limit 10'
 
+test_sql = 'select * from dbmodel.data_lianlian_idno_pro limit 100'
 
 X = np.array(fetch_from_hive(test_sql))
 
 print (X)
+
+y = [1]*10+[0]*90
 
 woe = cal_iv.WOE()
