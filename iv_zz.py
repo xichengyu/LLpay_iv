71932cc5e1e77be2f282a933dc49abf1734199d3
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-12 15:44:40 +0800

    add information value

--- iv_zz.py
+++ iv_zz.py
@@ -16,15 +16,13 @@ res = rd.read_local_data(localpath)
 new_nparray = rd.delete_str_column(res)
 
 y = new_nparray[:, -1]
-
 X = np.delete(new_nparray, -1, axis=1)
 
 rd.print_info(X.shape)
 rd.print_info(y.sum(), np.count_nonzero(y))
 
 cal_woe = WOE()
-
 res_woe, res_iv = cal_woe.woe(X, y)
 
-rd.print_info(res_woe)
-rd.print_info(res_iv)
+rd.print_info(res_woe, res_woe.shape)
+rd.print_info(res_iv, res_iv.shape)
