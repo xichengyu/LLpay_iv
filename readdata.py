ed77ad26a5f73f88c3afd4c8cfca9b6fc63ef1c3
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-12 12:57:00 +0800

    modify readdata.py

--- readdata.py
+++ readdata.py
@@ -4,7 +4,13 @@ import os
 import numpy as np
 
 
-def read_local_data(localpath):
+def read_local_data(localpath, default=-1.0):
+    """
+    read data from local place
+    :param default: default value used to replace non_type value
+    :param localpath: the path of data
+    :return: the new numpy array in which None_type values are replaced with -1.0
+    """
     files = os.listdir(localpath)
     res = []
     for file in files:
@@ -18,13 +24,24 @@ def read_local_data(localpath):
 
     res = np.copy(temp)
     for idx in range(res.shape[-1]):
-        res[:, idx][np.where(res[:, idx] == '\\N')[0]] = -1.0
-
+        res[:, idx][np.where(res[:, idx] == '\\N')[0]] = default
     print(res[0])
+    return res
 
-    pass
+
+def delete_nonint_column(nparray):
+    new_nparray = np.array([])
+    for idx in range(nparray.shape[-1]):
+        try:
+            np.append(new_nparray, res[:, idx].astype(float), 1)
+        except:
+            continue
+    return new_nparray
 
 
 if __name__ == "__main__":
     localpath = "../data_lianlian"
-    read_local_data(localpath)
+    res = read_local_data(localpath)
+    new_nparray = delete_nonint_column(res)
+
+    print(new_nparray[0])
