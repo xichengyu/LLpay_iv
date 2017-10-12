8145ae85371689b44b2dfb0642d2c6fb7c70dea1
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-12 12:13:55 +0800

    modify readdata.py

--- readdata.py
+++ readdata.py
@@ -1,14 +1,27 @@
 # coding=utf-8
 
 import os
+import numpy as np
 
 
 def read_local_data(localpath):
     files = os.listdir(localpath)
+    res = []
     for file in files:
         temp = open(localpath+"/%s" % file).readlines()
         print(type(temp))
         print(temp[0].split(), len(temp[0].split()))
+        for line in temp:
+            res.append(line.split())
+    print("total data: ", len(res))
+    temp = np.array(res)
+
+    res = np.copy(temp)
+    for idx in range(res.shape[-1]):
+        res[:, idx][np.where(res[:, idx] == "\N")[0]] = -1.0
+
+    print(res[0])
+
     pass
 
 
