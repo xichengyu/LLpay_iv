e190633cb55cba82577e0ad9675aa22057bab09f
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-14 19:44:40 +0800

    add information value

--- inputs.py
+++ inputs.py
@@ -14,6 +14,7 @@ import logging
 
 import numpy as np
 # import pandas as pd
+from sklearn.externals import joblib
 
 logging.basicConfig(level=logging.ERROR)
 logging.getLogger('../hivelog/pyhive.log').setLevel(logging.INFO)
@@ -96,6 +97,9 @@ def fetch_from_hive(sql):
 
 if __name__ == "__main__":
 
-    test_sql = "select * from dbmodel.data_zhangzhong_iv_sample limit 10"
+    test_sql = "select * from dbmodel.data_zhangzhong_iv_sample"
 
-    print(np.array(fetch_from_hive(test_sql)).shape)
+    data_zz_iv = fetch_from_hive(test_sql)
+
+    joblib.dump(data_zz_iv, "../data_zz_iv/zz_iv.dt")
+    print(np.array(data_zz_iv).shape)
