6a9621cf115c2970913d0bd7bb6d5ba0119cd892
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-10-15 21:38:52 +0800

    add information value

--- information_value/iv.py
+++ information_value/iv.py
@@ -153,19 +153,27 @@ class WOE(object):
                 logging.info("after: " + " ".join([str(i), str(set(x)), str(x)]))
         return np.array(temp).T
 
-    def discrete(self, x):
+    def discrete(self, x, n=5):
         '''
         Discrete the input 1-D numpy array using 5 equal percentiles
+        :param n: the number of discretion
         :param x: 1-D numpy array
         :return: discreted 1-D numpy array
         '''
         res = np.array([0] * x.shape[-1], dtype=int)
-        for i in range(5):
-            point1, point2 = stats.scoreatpercentile(x, [i*20, (i+1)*20])
-            x1 = x[np.where((x >= point1) & (x <= point2))]
-            mask = np.in1d(x, x1)
-            res[mask] = (i + 1)
-            logging.info("discrete: " + str(res) + str((point1, point2)))
+        x_temp = x[x != -1.0]
+        for i in range(1+n):
+            if i == 0:
+                x1 = x[np.where(x == -1.0)]
+                mask = np.in1d(x, x1)
+                res[mask] = (i + 1)
+                logging.info("discrete: " + str((-1.0, -1.0)))
+            else:
+                point1, point2 = stats.scoreatpercentile(x_temp, [(i-1)*100/n, i*100/n])
+                x1 = x[np.where((x >= point1) & (x <= point2))]
+                mask = np.in1d(x, x1)
+                res[mask] = (i + 1)
+                logging.info("discrete: " + str(res) + str((point1, point2)))
             logging.info("mask: " + str(mask))
         logging.info("discrete_main: " + str(res))
         return res
