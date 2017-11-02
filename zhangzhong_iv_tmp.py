794a9b1474d4907d1a79c5e47311a2d328b1e2be
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2017-11-02 21:25:18 +0800

    modify code

--- zhangzhong_iv_tmp.py
+++ zhangzhong_iv_tmp.py
@@ -19,9 +19,28 @@ if 0:
 else:
     localpath = "%s" % conf_info["raw_data_dump_path"]
     res = rd.load_local_data(localpath)
+
 joblib.dump(res, "conf/raw_data.dt")
+
+idno = res[:, 0]
+idno_dt = np.column_stack((idno, res[:, 1]))
+print(idno_dt.shape)
+
 new_nparray = rd.delete_str_column(res)
 joblib.dump(new_nparray, "conf/float_data.dt")
+
+
+tmp_idno_dt = []
+tmp_new_nparray = []
+
+for idx in range(new_nparray.shape[0]):
+    if sum(new_nparray[idx, :]) == -1*new_nparray.shape[-1]:
+        continue
+    tmp_idno_dt.append(idno_dt[idx, :])
+    tmp_new_nparray.append(new_nparray[idx, :])
+
+idno_dt = np.array(tmp_idno_dt)
+new_nparray = np.array(tmp_new_nparray)
 
 X = new_nparray
 joblib.dump(X, "conf/input_data.dt")
@@ -32,7 +51,15 @@ cal_woe = WOE()
 WOE.WOE_N = int(conf_info["iv_n"])
 X = cal_woe.feature_discretion(X)
 
+X = np.column_stack((idno_dt, X))
+
 joblib.dump(X, "conf/zhangzhong_result.dt")
 
+fout = open("result.csv", "w")
+for idx in range(X.shape[0]):
+    x_list = list(X[idx, :].astype(str))
+    fout.write(",".join(x_list)+"\n")
+
+fout.close()
 print(X)
 
