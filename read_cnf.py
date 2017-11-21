d71aa888c8699beb5108f320d7e2d69dee8cb0a6
xicy <xicy@yintong.com.cn>
2017-11-21 11:46:23 +0800

    modify conf/cnf.txt

--- read_cnf.py
+++ read_cnf.py
@@ -8,7 +8,7 @@ def get_conf_info(conf_path="./conf/cnf.txt"):
             if line[0] == "#" or line.strip() == "":
                 continue
             line = line.strip().split(" = ")
-            conf_dict[line[0]] = line[1]
+            conf_dict[line[0]] = " = ".join(line[1:])
     except UserWarning:
         print("target conf file not exists!")
     return conf_dict
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 git pull origin master
 python3.6 inputs.py
 echo "data query has done!"
+# python3.6 iv_zz.py
 nohup python3.6 iv_zz.py > test.log &
